65ce993c89d3e3236042de8c037f47d7c73af2f1
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-08-07 10:41:45 +0200

    Added all the additional files for working as well

---
+++ src/test/java/com/abdurrkhalid/cicdgithubactions/CiCdGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdurrkhalid.cicdgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdGitHubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
